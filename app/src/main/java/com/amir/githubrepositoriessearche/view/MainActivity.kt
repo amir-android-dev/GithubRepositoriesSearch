225861838a1d9909eff37a293a3d4f282d3e555d
amir-android-dev <amir-hosseini@gmx.at>
2022-07-06 09:52:52 +0200

    some small change in design

--- app/src/main/java/com/amir/githubrepositoriessearche/view/MainActivity.kt
+++ app/src/main/java/com/amir/githubrepositoriessearche/view/MainActivity.kt
@@ -1,18 +1,11 @@
 package com.amir.githubrepositoriessearche.view
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.amir.githubrepositoriessearche.R
 import com.amir.githubrepositoriessearche.databinding.ActivityMainBinding
 import com.amir.githubrepositoriessearche.model.GithubService
-import com.amir.githubrepositoriessearche.model.data.GithubResponse
 import com.amir.githubrepositoriessearche.model.data.Item
-import com.amir.githubrepositoriessearche.viewModel.ListViewModel
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -25,17 +18,19 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        fetchingDataAndPassingToRecyclerView()
     }
 
-    override fun onStart() {
-        super.onStart()
+
+    private fun fetchingDataAndPassingToRecyclerView(){
 
         binding.search.setOnClickListener {
             val q = binding.keyword.text.toString()
 
             // To show a new result every time you search
             repoList = mutableListOf()
-
+            binding.tvNothingFound.visibility = View.GONE
+            binding.progressBar.visibility = View.VISIBLE
             // LiveData use
             val response = GithubService().getRepository(q)
             response.observe(this) { response ->
@@ -44,19 +39,18 @@ class MainActivity : AppCompatActivity() {
                         repoList.add(item)
                     }
                 }
-
+                //setting up the recyclerView
+                binding.progressBar.visibility = View.GONE
                 repoAdapter = RepositoryListAdapter(repoList)
                 binding.viewResult.apply {
                     layoutManager =
                         LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
                     adapter = repoAdapter
                 }
-
                 if (repoAdapter.itemCount == 0) {
-                    Toast.makeText(this, "Nothing Found.", Toast.LENGTH_SHORT).show()
+                    binding.tvNothingFound.visibility = View.VISIBLE
                 }
             }
-
             // EditText clear
             binding.keyword.text?.clear()
         }
--- app/src/main/java/com/amir/githubrepositoriessearche/view/RepositoryListAdapter.kt
+++ app/src/main/java/com/amir/githubrepositoriessearche/view/RepositoryListAdapter.kt
@@ -9,6 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
+import androidx.cardview.widget.CardView
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.amir.githubrepositoriessearche.R
@@ -18,9 +19,7 @@ class RepositoryListAdapter(private val repositoryList: MutableList<Item>) :
     RecyclerView.Adapter<RepositoryListAdapter.ViewHolder>() {
 
     private val TAG = "RepositoryListAdapter"
-    var repName = ""
-    var owner = ""
-    var url = ""
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view =
@@ -33,7 +32,23 @@ class RepositoryListAdapter(private val repositoryList: MutableList<Item>) :
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         Log.d(TAG, "${repositoryList[position]}")
 
-        holder.bindInfo(repositoryList[position])
+        holder.bind(repositoryList[position])
+
+        if (position % 2 == 0) {
+            holder.cvContainer.setCardBackgroundColor(
+                ContextCompat.getColor(
+                    holder.itemView.context,
+                    R.color.white
+                )
+            )
+        } else {
+            holder.cvContainer.setCardBackgroundColor(
+                ContextCompat.getColor(
+                    holder.itemView.context,
+                    R.color.anti_flash_white
+                )
+            )
+        }
     }
 
     override fun getItemCount(): Int {
@@ -42,10 +57,13 @@ class RepositoryListAdapter(private val repositoryList: MutableList<Item>) :
     }
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun bindInfo(data: Item) {
-            val title = itemView.findViewById<TextView>(R.id.tv_title) // Repository title
-            val owner = itemView.findViewById<TextView>(R.id.tv_owner) // repository author
-            val url = itemView.findViewById<TextView>(R.id.tv_url) // HTTP URL
+
+        private val title = itemView.findViewById<TextView>(R.id.tv_title) // Repository title
+        private val owner = itemView.findViewById<TextView>(R.id.tv_owner) // repository author
+        private val url = itemView.findViewById<TextView>(R.id.tv_url) // HTTP URL
+        val cvContainer: CardView = itemView.findViewById(R.id.cv_container) // HTTP URL
+        fun bind(data: Item) {
+
 
             title.text = data.fullName
             owner.text = data.owner?.login
